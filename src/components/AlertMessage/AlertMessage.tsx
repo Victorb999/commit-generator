88886a4d4e0f5cc1904d2ca7a72c53e5ee69c855
Victor Araújo <victor.b_@hotmail.com>
2024-03-19 19:57:51 +0000

    feat: Added interface for AlertMessageProps and fixed useState initialization

--- src/components/AlertMessage/AlertMessage.tsx
+++ src/components/AlertMessage/AlertMessage.tsx
@@ -1,15 +1,23 @@
 import {
+  AlertDialog,
   AlertDialogAction,
   AlertDialogContent,
   AlertDialogDescription,
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogCancel,
 } from "@/components/ui/alert-dialog";
 
 import { CopyIcon } from "@radix-ui/react-icons";
 
-export const AlertMessage = ({ msg }: { msg: string }) => {
+interface AlertMessageProps {
+  msg: string;
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export const AlertMessage = ({ msg, open, setOpen }: AlertMessageProps) => {
   const handleCopyClick = async () => {
     try {
       const text = document.getElementById("commit-message");
@@ -20,25 +28,28 @@ export const AlertMessage = ({ msg }: { msg: string }) => {
   };
 
   return (
-    <AlertDialogContent>
-      <AlertDialogHeader>
-        <AlertDialogTitle>Commit message:</AlertDialogTitle>
-        <AlertDialogDescription>
-          {msg && (
-            <div className="text-sm bg-gray-500 text-black p-2 rounded-md">
-              <p
-                dangerouslySetInnerHTML={{ __html: msg }}
-                id="commit-message"
-              />
-            </div>
-          )}
-        </AlertDialogDescription>
-      </AlertDialogHeader>
-      <AlertDialogFooter>
-        <AlertDialogAction onClick={handleCopyClick}>
-          <CopyIcon />
-        </AlertDialogAction>
-      </AlertDialogFooter>
-    </AlertDialogContent>
+    <AlertDialog open={open} onOpenChange={setOpen}>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Commit message:</AlertDialogTitle>
+          <AlertDialogDescription>
+            {msg && (
+              <div className="text-sm bg-gray-500 text-black p-2 rounded-md">
+                <p
+                  dangerouslySetInnerHTML={{ __html: msg }}
+                  id="commit-message"
+                />
+              </div>
+            )}
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogAction onClick={handleCopyClick}>
+            <CopyIcon />
+          </AlertDialogAction>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 };
--- src/components/MainForm/MainForm.tsx
+++ src/components/MainForm/MainForm.tsx
@@ -31,10 +31,7 @@ export const MainForm = () => {
     cardNumber: "",
   });
   const [msg, setMsg] = useState("");
-
-  useEffect(() => {
-    console.log(form);
-  }, [form]);
+  const [open, setOpen] = useState(false);
 
   const handleSubmit = (e: any) => {
     e.preventDefault();
@@ -42,6 +39,7 @@ export const MainForm = () => {
     ${form.description}<br /><br />
     Jira Ticket [${form.cardNumber}](https://vistosystem.atlassian.net/browse/${form.cardNumber})`;
     setMsg(msg);
+    setOpen(true);
     console.log(msg);
   };
 
@@ -56,110 +54,109 @@ export const MainForm = () => {
           <CardDescription>Gere sua mensagem de commit</CardDescription>
         </CardHeader>
         <CardContent>
-          <AlertDialog>
-            <form
-              className="flex gap-2 p-2 flex-col"
-              onSubmit={(e) => handleSubmit(e)}
-            >
-              <div className="flex gap-2 items-center">
-                <Select
-                  onValueChange={(value: any) =>
-                    setForm({ ...form, type: value })
-                  }
-                >
-                  <SelectTrigger>
-                    <SelectValue placeholder="Tipo" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    {Object.keys(commitTypes).map((type) => (
-                      <SelectItem value={type} key={type}>
-                        {type}
-                      </SelectItem>
-                    ))}
-                  </SelectContent>
-                </Select>
-                <TooltipInfo
-                  msg={
-                    form.type !== ""
-                      ? commitTypes[form.type as keyof typeof commitTypes]
-                      : "O tipo de commit"
-                  }
-                />
-              </div>
+          <form
+            className="flex gap-2 p-2 flex-col"
+            onSubmit={(e) => handleSubmit(e)}
+          >
+            <div className="flex gap-2 items-center">
+              <Select
+                onValueChange={(value: any) =>
+                  setForm({ ...form, type: value })
+                }
+              >
+                <SelectTrigger>
+                  <SelectValue placeholder="Tipo" />
+                </SelectTrigger>
+                <SelectContent>
+                  {Object.keys(commitTypes).map((type) => (
+                    <SelectItem value={type} key={type}>
+                      {type}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+              <TooltipInfo
+                msg={
+                  form.type !== ""
+                    ? commitTypes[form.type as keyof typeof commitTypes]
+                    : "O tipo de commit"
+                }
+              />
+            </div>
 
-              <div className="flex gap-2 items-center">
-                <Input
-                  type="text"
-                  name="system"
-                  placeholder="Sistema"
-                  required
-                  onChange={(e) => setForm({ ...form, system: e.target.value })}
-                />
-                <TooltipInfo msg={`O sistema que está alterando.`} />
-              </div>
+            <div className="flex gap-2 items-center">
+              <Input
+                type="text"
+                name="system"
+                placeholder="Sistema"
+                required
+                onChange={(e) => setForm({ ...form, system: e.target.value })}
+              />
+              <TooltipInfo msg={`O sistema que está alterando.`} />
+            </div>
 
-              <div className="flex gap-2 items-center">
-                <Input
-                  type="text"
-                  name="titleCommit"
-                  placeholder="Título"
-                  required
-                  onChange={(e) => setForm({ ...form, title: e.target.value })}
-                />
-                <TooltipInfo
-                  msg={`O título contém uma descrição sucinta da mudança:<br />
+            <div className="flex gap-2 items-center">
+              <Input
+                type="text"
+                name="titleCommit"
+                placeholder="Título"
+                required
+                onChange={(e) => setForm({ ...form, title: e.target.value })}
+              />
+              <TooltipInfo
+                msg={`O título contém uma descrição sucinta da mudança:<br />
 -use o imperativo, tempo presente: "mudança" não "mudou" nem "muda"<br />
 -não capitalize a primeira letra<br />
 -sem ponto (.) no final`}
-                />
-              </div>
+              />
+            </div>
 
-              <div className="flex gap-2 items-center">
-                <Textarea
-                  placeholder="Descrição"
-                  name="descriptionCommit"
-                  required
-                  onChange={(e) =>
-                    setForm({ ...form, description: e.target.value })
-                  }
-                />
-                <TooltipInfo
-                  msg={`Um corpo de mensagem de commit 
+            <div className="flex gap-2 items-center">
+              <Textarea
+                placeholder="Descrição"
+                name="descriptionCommit"
+                required
+                onChange={(e) =>
+                  setForm({ ...form, description: e.target.value })
+                }
+              />
+              <TooltipInfo
+                msg={`Um corpo de mensagem de commit 
                   mais longo PODE ser fornecido após o título, 
                   fornecendo informações contextuais 
                   adicionais sobre as alterações no código.               
                   Use para explicar "o que" e "porque" foi 
                   realizado essa modificação, ao invez de "como".`}
-                />
-              </div>
-              <div className="flex gap-2 items-center">
-                <Input
-                  type="text"
-                  name="cardNumber"
-                  placeholder="Card"
-                  required
-                  onChange={(e) =>
-                    setForm({ ...form, cardNumber: e.target.value })
-                  }
-                />
-                <TooltipInfo msg={`O número do card. (ex: NS-123) `} />
-              </div>
-              <AlertDialogTrigger
-                disabled={
-                  !form.type ||
-                  !form.system ||
-                  !form.title ||
-                  !form.description ||
-                  !form.cardNumber
+              />
+            </div>
+            <div className="flex gap-2 items-center">
+              <Input
+                type="text"
+                name="cardNumber"
+                placeholder="Card"
+                required
+                onChange={(e) =>
+                  setForm({ ...form, cardNumber: e.target.value })
                 }
-              >
-                <Button type="submit" className="mt-2">
-                  Gerar mensagem
-                </Button>
-                <AlertMessage msg={msg} />
-              </AlertDialogTrigger>
-            </form>
-          </AlertDialog>
+              />
+              <TooltipInfo msg={`O número do card. (ex: NS-123) `} />
+            </div>
+
+            <Button
+              type="submit"
+              className="mt-2"
+              disabled={
+                !form.type ||
+                !form.system ||
+                !form.title ||
+                !form.description ||
+                !form.cardNumber
+              }
+            >
+              Gerar mensagem
+            </Button>
+            <AlertMessage msg={msg} open={open} setOpen={setOpen} />
+          </form>
         </CardContent>
       </Card>
     </div>

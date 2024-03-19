55f5f02f16d87af9aa08520c4bf8a97855ddb6be
Victor Araújo <victor.b_@hotmail.com>
2024-03-19 20:05:10 +0000

    feat: Add ToggleTheme component to allow users to switch between light and dark themes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 import { MainForm } from "../components/MainForm/MainForm";
+import { ToggleTheme } from "../components/ToggleTheme/ToggleTheme";
 
 export default function Home() {
   return (
     <div>
+      <div className="absolute top-4 right-4">
+        <ToggleTheme />
+      </div>
       <MainForm />
     </div>
   );
